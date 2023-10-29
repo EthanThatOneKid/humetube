86342325eb9be93e3651cd237a2aef7dfc059dda
Karnikaa Velumani <karnikaavelumani@csu.fullerton.edu>
2023-10-29 07:26:31 -0700

    WIP
    Co-Authored-By: Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,7 @@
 import { parseVideoID } from "./lib/youtube/index.js";
 import { createInterval, getState, updateState } from "./lib/humetube/index.js";
-// import { ingest } from "./lib/humetube/ingest.js";
+
+const recordingEnabled = true;
 
 // https://stackoverflow.com/a/11598753
 chrome.runtime.onInstalled.addListener(async () => {
@@ -19,25 +20,56 @@ chrome.tabs.onActivated.addListener(async (info) => {
   const tabID = info.tabId;
   const tab = await chrome.tabs.get(tabID);
   const videoID = parseVideoID(tab.url);
-  const state = getState();
   if (!videoID) {
-    clearInterval(state.intervalID);
-    state.activeTabID = null;
-    state.intervalID = null;
-    updateState(state);
-    // ingest();
     return;
   }
 
-  // When the user navigates to a YouTube page, update the tab ID and video ID.
-  state.videoID = videoID;
-  state.activeTabID = info.tabId;
+  // Try to render the emotions timeline.
+  fetch(`https://humetube.deno.dev/emotions/${videoID}`)
+    .then(async (response) => {
+      if (!response.ok) {
+        return;
+      }
 
-  // If the interval is not already running, start it.
-  if (!state.intervalID) {
-    state.intervalID = createInterval();
+      
+
+      // Parse the JSON data.
+      const data = await response.json();
+      if (data.emotions.length === 0) {
+        return;
+      }
+
+      sendDataToContentScript(tabID, data);
+    });
+
+  if (recordingEnabled) {
+    const state = getState();
+    if (!videoID) {
+      clearInterval(state.intervalID);
+      state.activeTabID = null;
+      state.intervalID = null;
+      updateState(state);
+      // TODO: Ingest stashed data via ingest().
+      return;
+    }
+
+    // When the user navigates to a YouTube page, update the tab ID and video ID.
+    state.videoID = videoID;
+    state.activeTabID = info.tabId;
+
+    // If the interval is not already running, start it.
+    if (!state.intervalID) {
+      state.intervalID = createInterval();
+    }
+
+    // Update the state.
+    updateState(state);
   }
-
-  // Update the state.
-  updateState(state);
 });
+
+function sendDataToContentScript(tabID, data) {
+  chrome.tabs.sendMessage(tabID, {
+    action: "renderEmotionsTimeline",
+    data,
+  });
+}
