d3f764e7ee3df1e4798159ab6804d5fd284c91e4
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-11-01 17:17:20 -0700

    run `deno task all`

--- server/deps.ts
+++ server/deps.ts
@@ -1,2 +1,2 @@
-export { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
+export { load } from "https://deno.land/std@0.205.0/dotenv/mod.ts";
 export { ulid } from "https://deno.land/x/ulid@v0.3.0/mod.ts";
